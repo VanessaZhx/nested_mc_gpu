c002e59a106b810ea0f04fc2288fa3a23e4aa4c5
Vanessa <vanessa.zhxx@gmail.com>
2022-08-22 01:28:41 +0100

    minor change

--- kernel.cu
+++ kernel.cu
@@ -158,6 +158,8 @@ int main(int argc, char* argv[])
 		port_n, port_w,
 		risk_free
 	);
+
+	mc->optimise_init(combined_rng, barrier_early, same_rn);
 	mc->bond_init(bond_par, bond_c, bond_m, bond_y, sigma, 0);
 	mc->stock_init(stock_s0, stock_mu, stock_var, stock_x, 1);
 	mc->bskop_init(bskop_n, bskop_stocks, bskop_cov, bskop_k, bskop_w, bskop_t, 2);
